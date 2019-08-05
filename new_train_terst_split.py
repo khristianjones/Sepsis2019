e3759c6459cf718e12c764c2a3c3ded51db8af4b
Erik <erikhg7@gmail.com>
2019-08-05 13:43:40 -0600

    Finished program, and added the new .txt files
    Will not need to run this again, unless we need a new split

--- new_train_terst_split.py
+++ new_train_terst_split.py
@@ -9,37 +9,80 @@ import random
 
 #filename = os.path.join(dir, 'relative','path','to','file','you','want')
 
-total_list = []
+total_list1 = []
+
+#####################Creates train test for the first file, I believe this only works on windows###################
 for filename in os.listdir("Training2/trainingA/training/"):
     x = filename[1:7]
     x = x.lstrip("0")   #Strips Leading zeros
-    total_list.append(x)
+    total_list1.append(x)
 
-random.shuffle(total_list)
+random.shuffle(total_list1)
 
-x = len(total_list)
+x = len(total_list1)
 
 twenty = int(x*0.2)
 
 eighty = int(x*0.8)
 print(twenty, eighty)
 
-train = total_list[0: eighty]
-test = total_list[eighty: x]
+train_setA = total_list1[0: eighty]
+test_setA = total_list1[eighty: x]
 
-f=open("new_test.txt", "a")
+f=open("train_setA.txt", "a")
 i=0
-for i in range(len(test)):
+for i in range(len(test_setA)):
     
-    f.write(test[i])
+    f.write(test_setA[i])
+    f.write("\t")
+    i+=1
+f.close()
+
+f=open("test_setA.txt", "a")
+i=0
+for i in range(len(test_setA)):
+    
+    f.write(test_setA[i])
     f.write("\t")
     i+=1
 f.close()
 
 
+#####################Creates train test for the second file###################
+total_list2 = []
+for filename in os.listdir("Training2/trainingB/training_setB/"):
+    x = filename[1:7]
+    total_list2.append(x)
 
+random.shuffle(total_list2)
 
+x = len(total_list2)
 
+twenty = int(x*0.2)
+
+eighty = int(x*0.8)
+print(twenty, eighty)
+
+train_setB = total_list2[0: eighty]
+test_setB = total_list2[eighty: x]
+
+f=open("train_setB.txt", "a")
+i=0
+for i in range(len(train_setB)):
+    
+    f.write(train_setB[i])
+    f.write("\t")
+    i+=1
+f.close()
+
+f=open("test_setB.txt", "a")
+i=0
+for i in range(len(test_setB)):
+    
+    f.write(test_setB[i])
+    f.write("\t")
+    i+=1
+f.close()
 
 
 
